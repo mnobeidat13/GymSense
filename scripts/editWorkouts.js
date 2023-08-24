e50aa0ad3d666a9303b2940aa6fc56f5e8fa122c
Mohammad Nur Aldeen Obeidat <32016587+mnobeidat13@users.noreply.github.com>
2023-08-25 02:09:50 +0400

    .

--- scripts/editWorkouts.js
+++ scripts/editWorkouts.js
@@ -7,12 +7,21 @@ const setWorkoutTabButton = document.getElementById('set-workout-tab-button')
 const newWorkoutSetButton = document.getElementById('create-new-workout-set-button')
 const newWorkoutButton = document.getElementById('create-new-workout-button')
 const addImageButton = document.getElementById('add-image-button')
-const closeButton = document.getElementById('new_workout-div-close-button')
+const closeButton = document.getElementById('new-workout-div-close-button')
 const closeButtonSet = document.getElementById('new_workout-set-div-close-button')
+
+const userWorkoutsTabButton = document.getElementById('user-workouts-tab-button')
+const preBuiltWorkoutsTabButton = document.getElementById('pre-built-workouts-tab-button')
+const cancelCreateSetWorkoutButton = document.getElementById('cancel-create-set-workout-button')
+
 
 //Divs Queries
 const singleWorkoutTabContent = document.getElementById('single-workout-tab-content')
 const setWorkoutTabContent = document.getElementById('set-workout-tab-content')
+
+const userWorkoutsTabContent = document.getElementById('user-workouts-tab-content')
+const preBuiltWorkoutsTabContent = document.getElementById('pre-built-workouts-tab-content')
+
 const newWorkoutDiv = document.getElementById('create-new-workout-div')
 const fileInput = document.getElementById('fileInput');
 const imagePreview = document.getElementById('imagePreview');
@@ -22,13 +31,22 @@ const userCreatedWorkoutsContainer = document.getElementById('user-created-worko
 const userCreatedWorkoutSetContainer = document.getElementById('user-created-workouts-set-container')
 const newWorkoutSetDiv = document.getElementById('create-new-workout-set-div')
 
+const userCreatedWorkoutsContainerCreatePlan = document.getElementById('user-created-workouts-container-create-plan')
+const preBuiltWorkoutsContainerCreatePlan = document.getElementById('pre-built-workouts-container-create-plan')
+
+
 //Toggling Tabs and Divs
 singleWorkoutTabButton.classList.add('active')
 // setWorkoutTabButton.classList.add('active')
+userWorkoutsTabButton.classList.add('active')
+
 setWorkoutTabContent.classList.add('hidden')
 // singleWorkoutTabContent.classList.add('hidden')
 newWorkoutDiv.classList.add('hidden')
 newWorkoutSetDiv.classList.add('hidden')
+
+// userWorkoutsTabContent.classList.add('hidden')
+preBuiltWorkoutsTabContent.classList.add('hidden')
 
 
 //Data Samples
@@ -40,11 +58,22 @@ const workoutSetData = [
     ['resources/exerciseIcon.png', 'Upper Body', 'Chest/ Biceps'],
     ['resources/exerciseIcon.png', 'Lower Body', 'Legs']]
 
+const createPlanWorkoutData1 = [['resources/benchPress.gif', 'Bench Press', 4, 12],
+    ['resources/inclineDumbbellPress.gif', 'Incline Dumbbell Press', 4, 12],
+    ['resources/chestPress.gif', 'Chest Press', 4, 12]]
+
+const createPlanWorkoutData2 = [['resources/shoulderPress.gif', 'Shoulder Press', 4, 12],
+['resources/lateralRaise.gif', 'Lateral Raise', 4, 12],
+['resources/tricepsPushdown.gif', 'Triceps Rope Pushdown', 4, 12],
+['resources/barbellTriceps.gif', 'Triceps Barbell', 4, 12]]
+
 workoutData.forEach(workout => {
     const [imageSrc, workoutName, numberOfSets, numberOfReps] = workout;
     var card = createSmallWorkoutCard(imageSrc, workoutName, numberOfSets, numberOfReps);
     userCreatedWorkoutsContainer.appendChild(card);
 });
+
+
 
 workoutSetData.forEach(workout => {
     const [imageSrc, bodyTarget, muscleTarget] = workout;
@@ -52,6 +81,17 @@ workoutSetData.forEach(workout => {
     userCreatedWorkoutSetContainer.appendChild(card);
 });
 
+createPlanWorkoutData1.forEach(workout => {
+    const [imageSrc, workoutName, numberOfSets, numberOfReps] = workout;
+    var card = createSmallWorkoutCard(imageSrc, workoutName, numberOfSets, numberOfReps);
+    userCreatedWorkoutsContainerCreatePlan.appendChild(card);
+});
+
+createPlanWorkoutData2.forEach(workout => {
+    const [imageSrc, workoutName, numberOfSets, numberOfReps] = workout;
+    var card = createSmallWorkoutCard(imageSrc, workoutName, numberOfSets, numberOfReps);
+    preBuiltWorkoutsContainerCreatePlan.appendChild(card);
+});
 
 //Attaching Event Listners
 singleWorkoutTabButton.addEventListener('click', function(event){
@@ -66,6 +106,24 @@ setWorkoutTabButton.addEventListener('click', function(event){
     singleWorkoutTabButton.classList.remove('active')
     setWorkoutTabContent.classList.remove('hidden')
     singleWorkoutTabContent.classList.add('hidden')
+})
+
+preBuiltWorkoutsTabButton.addEventListener('click', function(event){
+    event.target.classList.add('active')
+    userWorkoutsTabButton.classList.remove('active')
+    preBuiltWorkoutsTabContent.classList.remove('hidden')
+    userWorkoutsTabContent.classList.add('hidden')
+})
+
+userWorkoutsTabButton.addEventListener('click', function(event){
+    event.target.classList.add('active')
+    preBuiltWorkoutsTabButton.classList.remove('active')
+    userWorkoutsTabContent.classList.remove('hidden')
+    preBuiltWorkoutsTabContent.classList.add('hidden')
+})
+
+cancelCreateSetWorkoutButton.addEventListener('click', function(e){
+    newWorkoutSetDiv.classList.add('hidden')
 })
 
 newWorkoutButton.addEventListener('click', ()=>{
